30989cade136b51cbd4394e0dde6ba7dbd8c9747
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2025-01-24 15:34:40 +0700

    Create your first app

--- simple-app/app/index.tsx
+++ simple-app/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View ,StyleSheet} from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'#25292e',
+  },
+  text:{
+    color:'#fff',
+    fontSize: 20,
+  }
+});
