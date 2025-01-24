19de8aef4102a8b01586848c5456414979a7f8e2
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2025-01-24 15:48:15 +0700

    Chapter 2: Add navigation

--- simple-app/app/(tabs)/index.tsx
+++ simple-app/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View ,StyleSheet} from "react-native";
-
+import { Link } from "expo-router";
 export default function Index() {
   return (
     <View style={styles.container}
     >
-      <Text>Home screen</Text>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>Go to about</Link>
     </View>
   );
 }
@@ -19,5 +20,10 @@ const styles = StyleSheet.create({
   text:{
     color:'#fff',
     fontSize: 20,
-  }
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 });
