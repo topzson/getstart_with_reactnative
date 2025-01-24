3a148d2e3626afafc6c534110810477c6f9c7dfd
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2025-01-24 16:33:13 +0700

    Chapter 4: Use an image picker

--- simple-app/app/(tabs)/index.tsx
+++ simple-app/app/(tabs)/index.tsx
@@ -3,15 +3,35 @@ import { Image } from 'expo-image';
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 const PlaceholderImage = require('@/assets/images/background-image.png');
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from 'react';
+
+
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  }
+
   return (
     <View style={styles.container}
     >
       <View style={styles.imageContainer}>
-      <ImageViewer imgSource={PlaceholderImage} />
+      <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
     
