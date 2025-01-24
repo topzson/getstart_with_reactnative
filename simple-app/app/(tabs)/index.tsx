d2b984e1ebf933b700f3d4515f9051b4886a0962
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2025-01-24 17:09:09 +0700

    Chapter 8: Handle platform differences The app does everything we set out for it to do, so it's time to shift our focus toward the purely aesthetic..

--- simple-app/app/(tabs)/index.tsx
+++ simple-app/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useState,useRef } from 'react';
+import { useState, useRef } from 'react';
 import * as MediaLibrary from 'expo-media-library';
 import { type ImageSource } from 'expo-image';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import domtoimage from 'dom-to-image';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -55,24 +56,42 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS === 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Saved!');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
-  return (
-    <GestureHandlerRootView style={styles.container}>
+
+return (
+  <GestureHandlerRootView style={styles.container}>
     <View style={styles.container}>
-    <View ref={imageRef} collapsable={false}>
+      <View ref={imageRef} collapsable={false}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
@@ -94,8 +113,8 @@ export default function Index() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
     </View>
-    </GestureHandlerRootView>
-  );
+  </GestureHandlerRootView>
+);
 }
 
 const styles = StyleSheet.create({
